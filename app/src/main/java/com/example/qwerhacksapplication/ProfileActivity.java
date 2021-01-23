fa17d3541f13595e2d62472de6239c3f6fc36b77
vincenty1111 <vincenty1111@berkeley.edu>
2021-01-23 15:30:40 -0800

    created navi bar

--- app/src/main/java/com/example/qwerhacksapplication/ProfileActivity.java
+++ app/src/main/java/com/example/qwerhacksapplication/ProfileActivity.java
@@ -6,30 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.firestore.DocumentReference;
 
-public class MainActivity extends AppCompatActivity {
-    private DocumentReference reff;
+public class ProfileActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        DocumentReference reff;
+        setContentView(R.layout.activity_profile);
         createNaviBar();
-
     }
     private void createNaviBar() {
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
-        bottomNav.setSelectedItemId(R.id.botnav_home);
+        bottomNav.setSelectedItemId(R.id.botnav_profile);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.botnav_home:
+                        Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent1);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.botnav_trivia:
                         Intent intent = new Intent(getApplicationContext(), TriviaActivity.class);
@@ -38,9 +36,6 @@ public class MainActivity extends AppCompatActivity {
                         return true;
 
                     case R.id.botnav_profile:
-                        Intent intent1 = new Intent(getApplicationContext(), ProfileActivity.class);
-                        startActivity(intent1);
-                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
--- app/src/main/java/com/example/qwerhacksapplication/TriviaActivity.java
+++ app/src/main/java/com/example/qwerhacksapplication/TriviaActivity.java
@@ -6,40 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.firestore.DocumentReference;
 
-public class MainActivity extends AppCompatActivity {
-    private DocumentReference reff;
+public class TriviaActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        DocumentReference reff;
+        setContentView(R.layout.activity_trivia);
         createNaviBar();
-
     }
     private void createNaviBar() {
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
-        bottomNav.setSelectedItemId(R.id.botnav_home);
+        bottomNav.setSelectedItemId(R.id.botnav_trivia);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.botnav_home:
+                        Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent1);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.botnav_trivia:
-                        Intent intent = new Intent(getApplicationContext(), TriviaActivity.class);
-                        startActivity(intent);
-                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.botnav_profile:
-                        Intent intent1 = new Intent(getApplicationContext(), ProfileActivity.class);
-                        startActivity(intent1);
+                        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                        startActivity(intent);
                         overridePendingTransition(0, 0);
                         return true;
                 }

930f5a43c2c605965fb14433de901321ce0a784a
vincenty1111 <vincenty1111@berkeley.edu>
2021-01-22 23:41:51 -0800

    added dependencies for google's firestore database

--- app/src/main/java/com/example/qwerhacksapplication/MainActivity.java
+++ app/src/main/java/com/example/qwerhacksapplication/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.firestore.DocumentReference;
 
+public class MainActivity extends AppCompatActivity {
+    private DocumentReference reff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DocumentReference reff;
     }
 }

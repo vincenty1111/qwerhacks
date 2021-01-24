007384ce7603cbcc6aa2aca7f3417c8fa0b039f3
Jaeyub Chung <jybchung@berkeley.edu>
2021-01-23 17:47:03 -0800

    pull for firestore

--- app/src/main/java/com/example/qwerhacksapplication/ResultActivity.java
+++ app/src/main/java/com/example/qwerhacksapplication/ResultActivity.java
@@ -6,20 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class TriviaActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
         createNaviBar();
-        createQuestion();
-        createAnswer();
     }
     private void createNaviBar() {
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
@@ -29,6 +25,9 @@ public class TriviaActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.botnav_trivia:
+                        Intent intent0 = new Intent(getApplicationContext(), TriviaActivity.class);
+                        startActivity(intent0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.botnav_forum:
                         Intent intent1 = new Intent(getApplicationContext(), ForumActivity.class);
@@ -45,27 +44,4 @@ public class TriviaActivity extends AppCompatActivity {
             }
         });
     }
-    private void createQuestion() {
-        String question = "ygygygy";
-        TextView quest = findViewById(R.id.quest1);
-        quest.setText(question);
-
-    }
-
-    private void createAnswer() {
-        String Ans1 = "A";
-        String Ans2 = "B";
-        String Ans3 = "C";
-        String Ans4 = "D";
-
-        Button butt1 = findViewById(R.id.Ans1);
-        Button butt2 = findViewById(R.id.Ans2);
-        Button butt3 = findViewById(R.id.Ans3);
-        Button butt4 = findViewById(R.id.Ans4);
-        butt1.setText(Ans1);
-        butt2.setText(Ans2);
-        butt3.setText(Ans3);
-        butt4.setText(Ans4);
-    }
-
 }
